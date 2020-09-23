71b59cccc7378deafcbdad8220107c24f80b9744
Sam Odle <sro27@cornell.edu>
2020-09-23 08:54:55 -0400

    First Commit

---
+++ ConsoleApp2/PrintNameSizeRange.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class PrintNameSizeRange
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Note: List of Built in Types used for this program came from: 
+             * https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
+             * 
+             */
+
+            Console.WriteLine("CSCI 6221 - Homework #2 - Sam Odle");
+            Console.WriteLine();
+
+            var basicTypeList = getTypeList();
+
+            foreach (object t in basicTypeList)
+            {
+                Console.WriteLine(getInfoString(t.GetType()));
+            }
+        }
+        private static string getInfoString(Type o)
+        {
+            var sizeInBytes = Marshal.SizeOf(o);
+            var name = o.ToString();
+
+            dynamic minVal;
+            dynamic maxVal;
+
+            var sizeInBits = sizeInBytes * 8;
+
+            if (name.Contains("U") || name.Equals("System.Byte"))
+            { //perhaps inelegant, but it's valid for this application. see docs refernced above
+                minVal = 0;
+                maxVal = Math.Pow(2, sizeInBits) - 1;
+            }
+            else
+            {
+                minVal = Math.Pow(2, sizeInBits - 1) * -1;
+                maxVal = Math.Pow(2, sizeInBits - 1) - 1;
+            }
+
+            return (o.ToString() + ", Size in Bytes: " + sizeInBytes + ", Range: " + minVal + " to " + maxVal);
+        }
+
+        private static List<object> getTypeList()
+        {
+            return new List<object>() { new bool(), new byte(), new sbyte(), new char(), new decimal(), new double(), new float(), new int(), new uint(), new long(), new ulong(), new short(), new ushort() };
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+             * Note: List of Built in Types used for this program came from: 
+             * https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/built-in-types
+             * 
+             * Sam Odle 9/23/2020
+             * 
+             */
+
+            Console.WriteLine("CSCI 6221 - Homework #3 - Sam Odle");
+            Console.WriteLine();
+
+            var basicTypeList = getTypeList();
+
+            foreach(object t in basicTypeList)
+            {
+                Console.WriteLine(getInfoString(t.GetType()));
+            }
+        }
+
+    }
+}
